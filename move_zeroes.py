8854cae343cf0894b56ef109c41a780c42eb6486
stoneyang <stoneyang0915@gmail.com>
2021-08-10 21:27:50 +0800

    Create move_zeroes.py

---
+++ move_zeroes.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        left = right = 0
+        while right < len(nums):
+            if nums[right] != 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+            right += 1
