a7dfc222266a1b375117bc896f9749101983eb3c
stoneyang <stoneyang0915@gmail.com>
2021-08-10 21:06:50 +0800

    Create max_consecutive_ones.py

---
+++ max_consecutive_ones.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def findMaxConsecutiveOnes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max_cnt = 0
+        cur_cnt = 0
+        for e in nums:
+            if e == 1:
+                cur_cnt += 1
+                max_cnt = cur_cnt
+            elif e == 0:
+                max_cnt = max(max_cnt,cur_cnt)
+                cur_cnt = 0
+        return max_cnt
